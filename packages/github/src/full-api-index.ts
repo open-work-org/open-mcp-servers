cc6538fb26e21777abf53b48432632e08289e2ed
Aboobaker Siddiq R <144536269+aboobakersiddiqr63@users.noreply.github.com>
2026-08-29 23:51:08 +0530

    chore(github): remove obsolete packages/github/src/full-api-index.ts

--- packages/github/src/full-api-index.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { resolveApiKey } from "./op-fallback.js";
-import { GitHubApiClient } from "./services/api.js";
-import { createGitHubMcpServer } from "./server-factory.js";
-
-resolveApiKey("GITHUB_PERSONAL_ACCESS_TOKEN", "op://Development/GitHub Personal Access Token/credential");
-const server = createGitHubMcpServer(new GitHubApiClient(process.env.GITHUB_PERSONAL_ACCESS_TOKEN ?? ""));
-await server.connect(new StdioServerTransport());
